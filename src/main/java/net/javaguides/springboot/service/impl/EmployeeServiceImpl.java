f3571fbd3ab14560b652823d7925d7864498e6ea
Sai10313 <gsaikumar19979@gmail.com>
2023-11-13 10:59:38 +0530

    Changed the properties file

--- src/main/java/net/javaguides/springboot/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/springboot/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package net.javaguides.springboot.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import net.javaguides.springboot.exception.ResourceNotFoundException;
@@ -12,7 +13,7 @@ import net.javaguides.springboot.service.EmployeeService;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
-	
+	@Autowired
 	private EmployeeRepository employeeRepository;
 
 	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
